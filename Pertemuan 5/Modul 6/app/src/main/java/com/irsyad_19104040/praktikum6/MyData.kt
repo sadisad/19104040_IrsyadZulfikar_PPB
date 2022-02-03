427e9224f69f7e8a5a5d0eb0b870ee61397d674c
isad <61278337+sadisad@users.noreply.github.com>
2022-02-03 20:14:50 +0700

    Add files via upload

---
+++ Pertemuan 5/Modul 6/app/src/main/java/com/irsyad_19104040/praktikum6/MyData.kt
@@ -0,0 +1,11 @@
+package com.irsyad_191040403.praktikum6
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class MyData(
+    var name : String,
+    var description : String,
+    var photo : String
+):Parcelable
