c7c3efe65845a21ae3115756abd78dd615db2422
isad <zulfikarirsyad@gmail.com>
2022-02-03 21:55:09 +0700

    Add files via upload

---
+++ Pertemuan 9/Modul10/app/src/main/java/com/angger_19104073/localdatabase/Quote.kt
@@ -0,0 +1,13 @@
+package com.irsyad_19104040.localdatabase
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class Quote(
+    var id: Int = 0,
+    var title: String? = null,
+    var description: String? = null,
+    var category: String? = null, var
+    date: String? = null
+):Parcelable
